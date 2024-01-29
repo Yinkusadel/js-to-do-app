ad07233cf2376351d44a8185fdf4dc6413c5e711
yinkusadel <adeleyeolayinka73@gmail.com>
2024-01-29 15:37:50 +0100

    feat: update checkbox and font style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
